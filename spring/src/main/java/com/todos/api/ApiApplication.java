aa6c840c73db76ed2ef06552024c3cddb6140e11
dragosh <dragos.stefanescu@ing.com>
2019-12-08 13:47:01 +0100

    scheduled jobs Spring

--- spring/src/main/java/com/todos/api/ApiApplication.java
+++ spring/src/main/java/com/todos/api/ApiApplication.java
@@ -2,11 +2,13 @@ package com.todos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 		SpringApplication.run(ApiApplication.class, args);
 	}
